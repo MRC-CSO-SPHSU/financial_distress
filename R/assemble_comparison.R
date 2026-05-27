20d33745d8f1629197f69f8f51a66537d15c50fb
Darwin Del Castillo <ddelcast@uw.edu>
2026-05-27 15:21:07 +0100

    fix: commented ltmle funct

--- R/assemble_comparison.R
+++ R/assemble_comparison.R
@@ -1,6 +1,14 @@
-assemble_comparison <- function(ltmle_results, mi_results, iptw_results) {
-  ltmle_results |>
-    dplyr::left_join(mi_results,   by = "intervention") |>
+# assemble_comparison <- function(ltmle_results, mi_results, iptw_results) {
+#  ltmle_results |>
+#    dplyr::left_join(mi_results,   by = "intervention") |>
+#    dplyr::left_join(iptw_results, by = "intervention") |>
+#    dplyr::relocate("intervention")
+#} 
+
+# Activate the above function once I make the whole pipeline work
+
+assemble_comparison <- function(mi_results, iptw_results) {
+  mi_results |>
     dplyr::left_join(iptw_results, by = "intervention") |>
     dplyr::relocate("intervention")
 }
