e74c5b1c8d3a9e589cd45dc3a9b7b7af335cb8b2
Darwin Del Castillo <ddelcast@uw.edu>
2026-06-15 20:29:55 +0100

    fix: call to iptw

--- R/assemble_comparison.R
+++ R/assemble_comparison.R
@@ -1,6 +1,5 @@
-assemble_comparison <- function(ltmle_results, mi_results, iptw_results) {
+assemble_comparison <- function(ltmle_results, mi_results) {
   ltmle_results |>
     dplyr::left_join(mi_results,   by = "intervention") |>
-    dplyr::left_join(iptw_results, by = "intervention") |>
     dplyr::relocate("intervention")
 }
