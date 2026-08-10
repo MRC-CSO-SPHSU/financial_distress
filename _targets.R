02b0342d56c8d9d124119c30a73684739ad90a76
Darwin Del Castillo <ddelcast@uw.edu>
2026-08-10 10:50:50 +0100

    drop: nnet from learners and tmle

--- _targets.R
+++ _targets.R
@@ -84,7 +84,7 @@ outcome_scale <- list(outcome = c("MCS", "PCS"))
 
 ## SuperLearner library for the TMLE Q- and g-models. SL.xgboost.tmle is the
 ## custom wrapper in R/sl_wrappers.R (re-scaled outcome (0,1) handling).
-sl_libs <- c("SL.mean", "SL.glm", "SL.gam", "SL.nnet", "SL.xgboost.tmle")
+sl_libs <- c("SL.mean", "SL.glm", "SL.gam", "SL.xgboost.tmle")
 
 # ------------------------------- DAG -----------------------------------
 # ---- Loop for each outcome scale (MCS, PCS) using static branching ----
